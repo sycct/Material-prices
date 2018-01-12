94d632ee2871fd3d035f0c704da41017310ee6c1
Huang Bing <huang_9119596@163.com>
2018-01-12 14:23:45 +0800

    2018-1-12 Moment function and footer finished.

--- app/Material prices.py
+++ app/Material prices.py
@@ -1,11 +1,14 @@
 from flask import Flask , render_template
+from flask.ext.moment import Moment
+from datetime import datetime
 
 app = Flask(__name__)
-
+moment=Moment(app)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html',
+                           current_time=datetime.utcnow())
 
 
 if __name__ == '__main__':
