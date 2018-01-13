f085b031bbced9b78efa894ed6101e6c50567664
Huang Bing <huang_9119596@163.com>
2018-01-13 15:33:45 +0800

    2018-1-12 Add Error Pages.

--- app/Material prices.py
+++ app/Material prices.py
@@ -1,15 +1,24 @@
 from flask import Flask , render_template
 from flask.ext.moment import Moment
 from datetime import datetime
+from flask.ext.bootstrap import Bootstrap
 
 app = Flask(__name__)
 moment=Moment(app)
+bootstrap=Bootstrap(app)
 
 @app.route('/')
 def index():
     return render_template('index.html',
                            current_time=datetime.utcnow())
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'),404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'),500
 
 if __name__ == '__main__':
     app.run()
