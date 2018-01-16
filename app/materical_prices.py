4697772d0f9a50e8e5deb800bd9c296773a2e3c5
Huang Bing <huang_9119596@163.com>
2018-01-16 17:31:08 +0800

    2018-1-12 Project Used Mysql Server Config.

--- app/materical_prices.py
+++ app/materical_prices.py
@@ -5,11 +5,16 @@ from flask.ext.bootstrap import Bootstrap
 from flask.ext.wtf import Form
 from wtforms import StringField,SubmitField
 from wtforms.validators import Required
+from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 moment=Moment(app)
 bootstrap=Bootstrap(app)
 app.config['SECRET_KEY']='mLZXlBhl7hoV39xt6PUsJI8N3UUF8r575E77953YH7hIDOv12Yw9kua4nU75xybyyFDfSM6ZO4UPW4UO69e98lisAItyUTkI2TbplZTsDdfdM9ZG'
+
+app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN']=True
+
+db=SQLAlchemy(app)
 
 @app.route('/')
 def index():
@@ -29,3 +34,22 @@ def internal_server_error(e):
 
 if __name__ == '__main__':
     app.run()
+
+class Role(db.Model):
+    __tablename__='roles'
+    id=db.Column(db.Integer,primary_key=True)
+    name=db.Column(db.String(64),unique=True)
+    users=db.relationship('User',backref='role')
+
+    def __repr__(self):
+        return '<Role %r>' % self.name
+
+class User(db.Model):
+    __tablename__='users'
+    id=db.Column(db.Integer,primary_key=True)
+    username=db.Column(db.String(64),unique=True,index=True)
+    role_id=db.Column(db.Integer,db.ForeignKey('roles.id'))
+
+    def __repr__(self):
+        return '<User %r>'% self.username
+
