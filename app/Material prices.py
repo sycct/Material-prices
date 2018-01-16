8ff374dfe7d96c8c1f4420f7e8c2a9f88c4556a0
Huang Bing <huang_9119596@163.com>
2018-01-16 14:25:10 +0800

    2018-1-12 Add Plugins and style scripts.

--- app/Material prices.py
+++ app/Material prices.py
@@ -2,15 +2,22 @@ from flask import Flask , render_template
 from flask.ext.moment import Moment
 from datetime import datetime
 from flask.ext.bootstrap import Bootstrap
+from flask.ext.wtf import Form
+from wtforms import StringField,SubmitField
+from wtforms.validators import Required
 
 app = Flask(__name__)
 moment=Moment(app)
 bootstrap=Bootstrap(app)
+app.config['SECRET_KEY']='mLZXlBhl7hoV39xt6PUsJI8N3UUF8r575E77953YH7hIDOv12Yw9kua4nU75xybyyFDfSM6ZO4UPW4UO69e98lisAItyUTkI2TbplZTsDdfdM9ZG'
 
 @app.route('/')
 def index():
     return render_template('index.html',
                            current_time=datetime.utcnow())
+@app.route('/login')
+def login():
+    return  render_template('login.html')
 
 @app.errorhandler(404)
 def page_not_found(e):
