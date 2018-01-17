0028f3a893b1d157c52513045b7b6b70708b657d
Huang Bing <huang_9119596@163.com>
2018-01-17 22:27:29 +0800

    2018-1-17 Home edit Migrate db other files.

--- app/__init__.py
+++ app/__init__.py
@@ -1,15 +1,22 @@
 from flask import Flask, render_template
-from flask.ext.bootstrap import Bootstrap
-from flask.ext.mail import Mail
-from flask.ext.moment import Moment
-from flask.ext.sqlalchemy import SQLAlchemy
+from flask_bootstrap import Bootstrap
+from flask_mail import Mail
+from flask_moment import Moment
+from flask_sqlalchemy import SQLAlchemy
 from config import config
+from flask_pagedown import PageDown
+from flask_login import LoginManager
 
 bootstrap = Bootstrap()
 mail = Mail()
 moment = Moment()
 db = SQLAlchemy()
+pagedown = PageDown()
 
+
+login_manager = LoginManager()
+login_manager.session_protection = 'strong'
+login_manager.login_view = 'auth.login'
 
 def create_app(config_name):
     app = Flask(__name__)
--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,18 @@
 from datetime import datetime
-from flask import render_template,sessions,redirect,url_for
+from flask import render_template, sessions, redirect, url_for
 
 from . import main
-from . forms import NameForm
+from .forms import NameForm
 from .. import db
 from ..models import User
+
+
+@main.route('/', methods=['GET', 'POST'])
+def index():
+    form = NameForm()
+    if form.validate_on_submit():
+        return redirect(url_for('.index'))
+    return render_template('index.html',
+                           form=form, name=sessions.get('name'),
+                           known=sessions.get('known', False),
+                           current_time=datetime.utcnow())
