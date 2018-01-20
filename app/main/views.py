c12870605cf0ba35133d7f283c58fc62158f8d5a
Huang Bing <huang_9119596@163.com>
2018-01-20 17:47:26 +0800

    2018-1-20 Fix some problems

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 from flask import render_template, sessions, redirect, url_for
-
+from flask_moment import Moment
 from . import main
 from .forms import NameForm
 from .. import db
@@ -9,10 +9,4 @@ from ..models import User
 
 @main.route('/', methods=['GET', 'POST'])
 def index():
-    form = NameForm()
-    if form.validate_on_submit():
-        return redirect(url_for('.index'))
-    return render_template('index.html',
-                           form=form, name=sessions.get('name'),
-                           known=sessions.get('known', False),
-                           current_time=datetime.utcnow())
+    return render_template('index.html',current_time=datetime.utcnow())
