c53a6be3169700942f7eb46040828eee7eacaa08
Huang Bing <huang_9119596@163.com>
2018-01-24 17:07:38 +0800

    2018-1-24 add Unit tests.

--- manage.py
+++ manage.py
@@ -10,6 +10,14 @@ manager = Manager(app)
 migrate = Migrate(app, db)
 
 
+@manager.command
+def test():
+    """Run the unit tests."""
+    import unittest
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner(verbosity=2).run(tests)
+
+
 def make_shell_context():
     return dict(app=app, db=db, User=User, Role=Role)
 
