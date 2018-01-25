6291d34895cdf092be5f75f2a778d15cc5b5f4cc
Huang Bing <huang_9119596@163.com>
2018-01-25 17:35:27 +0800

    2018-1-12 Edit Bug.

--- manage.py
+++ manage.py
@@ -22,7 +22,7 @@ def make_shell_context():
     return dict(app=app, db=db, User=User, Role=Role)
 
 
-manager.add_command("shell", Shell(make_context=make_shell_context()))
+manager.add_command("shell", Shell(make_context=make_shell_context))
 manager.add_command("db", MigrateCommand)
 
 if __name__ == '__main__':
