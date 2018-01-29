24a0909f5d13031f6796e29120c89c36dedadcce
Huang Bing <huang_9119596@163.com>
2018-01-29 15:28:05 +0800

    2018-1-29 Add Send Email.

---
+++ app/email.py
@@ -0,0 +1,20 @@
+from threading import Thread
+from flask_mail import Message
+from . import mail
+from flask import current_app, render_template
+
+
+def send_async_email(app, msg):
+    with app.app_context():
+        mail.send(msg)
+
+
+def send_email(to, suject, template, **kwargs):
+    app = current_app._get_current_object()
+    msg = Message(app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + suject, sender=app.config['FLASKY_MAIL_SENDER'],
+                  recipients=[to])
+    msg.body = render_template(template + '.txt', **kwargs)
+    msg.html = render_template(template + '.html', **kwargs)
+    thr = Thread(target=send_async_email, args=[app, msg])
+    thr.start()
+    return thr
