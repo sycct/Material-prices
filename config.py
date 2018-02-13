a2f761f1d605c9879a048fdacd7171a36bda8435
Huang Bing <huang_9119596@163.com>
2018-02-13 16:04:35 +0800

    2018-2-13 update package.

--- config.py
+++ config.py
@@ -7,14 +7,16 @@ class Config:
     SECRET_KEY = os.environ.get(
         'SECRET_KEY') or 'mLZXlBhl7hoV39xt6PUsJI8N3UUF8r575E77953YH7hIDOv12Yw9kua4nU75xybyyFDfSM6ZO4UPW4UO69e98lisAItyUTkI2TbplZTsDdfdM9ZG'
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
-    MAIL_SERVER = os.environ.get('MAIL_SERVER', 'smtp.googlemail.com')
-    MAIL_PORT = int(os.environ.get('MAIL_PORT', '587'))
-    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS', 'true').lower() in \
-                   ['true', 'on', '1']
+    # 163 mail config
+    MAIL_SERVER = os.environ.get('MAIL_SERVER', 'smtp.163.com')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT', '465'))
+    #MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS', 'true').lower() in \
+                   #['true', 'on', '1']
+    MAIL_USE_SSL = os.environ.get('MAIL_USE_SSL', 'true')
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
-    FLASKY_MAIL_SENDER = 'Flasky Admin <flasky@example.com>'
+    FLASKY_MAIL_SENDER = 'Flasky Admin <huang_9119596@163.com>'
     FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
     # Bootstrap flask config
     BOOTSTRAP_USE_MINIFIED = True
