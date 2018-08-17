839dcf2b2db0d671c5fb04c7cc3703a74b3eb4ec
pdf-lib.org <huang_9119596@163.com>
2018-08-17 17:33:30 +0800

    2018-8-29 add admin_pro_value page.

--- app/manage/forms.py
+++ app/manage/forms.py
@@ -1,10 +1,10 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, TextAreaField, BooleanField, SelectField, \
-    SubmitField, PasswordField
+    SubmitField, PasswordField, RadioField
 from wtforms.validators import DataRequired, Length, Email, Regexp, EqualTo
 from flask_wtf.file import FileField
 from wtforms.ext.sqlalchemy.fields import QuerySelectField
-from ..models import MaterialClassification, ClassificationCatalog, MaterialItem
+from ..models import MaterialClassification, ClassificationCatalog, MaterialItem, MaterialProductName
 from .. import db
 
 
@@ -53,7 +53,7 @@ class AddBrandForm(FlaskForm):
 
     Brand_name = StringField("品牌名称：", validators=[Length(0, 64), DataRequired()])
     Brand_to_Item = QuerySelectField(label=u'材料名称', validators=[DataRequired()], query_factory=query_factory,
-                                        get_pk=get_pk)
+                                     get_pk=get_pk)
     submit = SubmitField('保存更改')
 
 
@@ -73,3 +73,36 @@ class AddMaterialItemForm(FlaskForm):
     # Item_to_ChildItem=QuerySelectField(label=u'子品牌名称', query_factory=child_query_factory,
     #                                     get_pk=get_pk)
     submit = SubmitField('保存更改')
+
+
+class AddMaterialPropertyNameForm(FlaskForm):
+    def query_factory(*args):
+        return [r.i_name for r in db.session.query(MaterialItem).all()]
+
+    def get_pk(obj):
+        return obj
+
+    property_name = StringField("属性名称：", validators=[Length(0, 50)])
+    item_to_pro_name = QuerySelectField(label=u'所属类别：', validators=[DataRequired()], query_factory=query_factory,
+                                        get_pk=get_pk)
+    pro_has_otherName = RadioField('是否有别名： ', choices=[('0', '是'), ('1', '否')])
+    pro_has_color = RadioField('是否有颜色属性： ', choices=[('0', '是'), ('1', '否')])
+    pro_has_enum = RadioField('是否可枚举： ', choices=[('0', '是'), ('1', '否')])
+    pro_has_input = RadioField('属性录入方式： ', choices=[('0', '手工录入'), ('1', '从下面的列表中选择（一行代表一个可选值）')])
+    pro_is_key = RadioField('属性值可选值列表： ', choices=[('0', '是'), ('1', '否')])
+    pro_is_sale = RadioField('是否支持手动新增： ', choices=[('0', '是'), ('1', '否')])
+    pro_is_must = RadioField('是否必选字段： ', choices=[('0', '是'), ('1', '否')])
+    submit = SubmitField('保存更改')
+
+
+class AddMaterialPropertyValueForm(FlaskForm):
+    def query_factory(*args):
+        return [r.i_name for r in db.session.query(MaterialProductName).all()]
+        return [r.i_name for r in db.session.query(MaterialItem).all()]
+
+    def get_pk(obj):
+        return obj
+
+    property_value = StringField("属性值： ", validators=[Length(0, 50)])
+    value_to_pro_name = QuerySelectField(label=u'属性名称： ', validators=[DataRequired()], query_factory=query_factory,
+                                         get_pk=get_pk)
