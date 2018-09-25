c331b9b2081f7b121a2a143eec7ab27198e49b3c
pdf-lib.org <huang_9119596@163.com>
2018-09-25 16:55:33 +0800

    2018-9-26 edit slider and use CDN.

--- app/__init__.py
+++ app/__init__.py
@@ -1,20 +1,23 @@
 # -*- coding: UTF-8 -*-
-from flask import Flask, render_template
-from flask_bootstrap import Bootstrap
+from flask import Flask
+from flask_bootstrap import Bootstrap, WebCDN, ConditionalCDN, BOOTSTRAP_VERSION, JQUERY_VERSION, HTML5SHIV_VERSION, \
+    RESPONDJS_VERSION
 from flask_mail import Mail
 from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
 from config import config
 from flask_pagedown import PageDown
 from flask_login import LoginManager
-from flask_wtf.csrf import CsrfProtect
+from flask_wtf.csrf import CSRFProtect
+from flask_assets import Environment, Bundle
 
 bootstrap = Bootstrap()
 mail = Mail()
 moment = Moment()
 db = SQLAlchemy()
 pagedown = PageDown()
-csrf = CsrfProtect()
+csrf = CSRFProtect()
+assets = Environment()
 
 login_manager = LoginManager()
 login_manager.session_protection = 'strong'
@@ -34,6 +37,11 @@ def create_app(config_name):
     moment.init_app(app)
     db.init_app(app)
 
+    # 初始化assets
+    assets.init_app(app)
+    js = Bundle('jquery.js', 'base.js', 'widgets.js', output='gen/packed.js')
+    assets.register('js_all', js)
+
     # csrf protect
     csrf.init_app(app)
 
@@ -49,4 +57,25 @@ def create_app(config_name):
     from .auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint, url_prefix='/auth')
 
+    # use CDN
+    app.config['BOOTSTRAP_SERVE_LOCAL'] = False
+    change_cdn_domestic(app)
+
     return app
+
+
+def change_cdn_domestic(tar_app):
+    static = tar_app.extensions['bootstrap']['cdns']['static']
+    local = tar_app.extensions['bootstrap']['cdns']['local']
+
+    def change_one(tar_lib, tar_ver, fallback):
+        tar_js = ConditionalCDN('BOOTSTRAP_SERVE_LOCAL', fallback,
+                                WebCDN('//cdn.bootcss.com/' + tar_lib + '/' + tar_ver + '/'))
+        tar_app.extensions['bootstrap']['cdns'][tar_lib] = tar_js
+
+    libs = {'jquery': {'ver': JQUERY_VERSION, 'fallback': local},
+            'bootstrap': {'ver': BOOTSTRAP_VERSION, 'fallback': local},
+            'html5shiv': {'ver': HTML5SHIV_VERSION, 'fallback': static},
+            'respond.js': {'ver': RESPONDJS_VERSION, 'fallback': static}}
+    for lib, par in libs.items():
+        change_one(lib, par['ver'], par['fallback'])
